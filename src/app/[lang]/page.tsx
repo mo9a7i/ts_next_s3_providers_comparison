ac31dd7d0369cffe0a39926232232bddfb11df27
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2025-01-27 20:08:24 +0300

    Update page.tsx

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,14 +1,16 @@
-import { JSX } from 'react';
-import { getDictionary } from './dictionaries'
+import { JSX } from "react";
+import { getDictionary } from "./dictionaries";
 import { CalculatorClient } from "@/components/calculator-client";
 
 type PageProps = {
     params: {
-      lang: 'en-US' | 'ar-SA';
+        lang: "en-US" | "ar-SA";
     };
-  };
-  
-  export default async function Page({ params: { lang } }: PageProps): Promise<JSX.Element> {
-      const dictionary = await getDictionary(lang);
+};
+
+export default async function Page({ 
+    params 
+}: PageProps): Promise<JSX.Element> {
+    const dictionary = await getDictionary(params.lang);
     return <CalculatorClient dictionary={dictionary} />;
 }
